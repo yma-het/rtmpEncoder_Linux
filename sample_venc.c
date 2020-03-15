56361b93395c7a4fe40b9ab1fe3010082f400322
YMA HET <yma.het@gmail.com>
2020-03-15 11:48:23 -0700

    Reverted almost all sample_venc.c to vanilla state

--- sample_venc.c
+++ sample_venc.c
@@ -29,7 +29,6 @@ HI_U32 g_u32BlkCnt = 4;
 #ifdef hi3518ev200
 
 HI_U32 g_u32BlkCnt = 4;
-HI_U32 g_u32RTMPBlkCnt = 8;
 
 #endif
 
@@ -98,7 +97,7 @@ extern int g_s32Quit;
 void *SAMPLE_VENC_1080P_CLASSIC(HI_VOID *arg)
 {
     PAYLOAD_TYPE_E enPayLoad[3]= {PT_H264, PT_H264,PT_H264};
-    PIC_SIZE_E enSize[3] = {PIC_HD1080 ,PIC_HD720 ,PIC_QVGA};
+    PIC_SIZE_E enSize[3] = {PIC_HD720, PIC_HD720, PIC_HD720};
 	HI_U32 u32Profile = 0;
 	
     VB_CONF_S stVbConf;
@@ -146,7 +145,6 @@ void *SAMPLE_VENC_1080P_CLASSIC(HI_VOID *arg)
 #ifdef hi3518ev201
 	s32ChnNum = 1;
 #endif
-    s32ChnNum=1; // Do we actually need this dirty hack for gc1024?
 	printf("s32ChnNum = %d\n",s32ChnNum);
 
     stVbConf.u32MaxPoolCnt = 128;
@@ -218,7 +216,7 @@ void *SAMPLE_VENC_1080P_CLASSIC(HI_VOID *arg)
 	    stVpssGrpAttr.bHistEn = HI_FALSE;
 	    stVpssGrpAttr.bDciEn = HI_FALSE;
 	    stVpssGrpAttr.enDieMode = VPSS_DIE_MODE_NODIE;
-	    stVpssGrpAttr.enPixFmt = PIXEL_FORMAT_YUV_SEMIPLANAR_420; // Different format. So far considering that newest is corrcrt one
+	    stVpssGrpAttr.enPixFmt = PIXEL_FORMAT_YUV_SEMIPLANAR_420;
 		
 	    s32Ret = SAMPLE_COMM_VPSS_StartGroup(VpssGrp, &stVpssGrpAttr);
 	    if (HI_SUCCESS != s32Ret)
@@ -237,7 +235,7 @@ void *SAMPLE_VENC_1080P_CLASSIC(HI_VOID *arg)
 		VpssChn = 0;
 	    stVpssChnMode.enChnMode      = VPSS_CHN_MODE_USER;
 	    stVpssChnMode.bDouble        = HI_FALSE;
-	    stVpssChnMode.enPixelFormat  = PIXEL_FORMAT_YUV_SEMIPLANAR_420; // Different format. So far considering that newest is corrcrt one
+	    stVpssChnMode.enPixelFormat  = PIXEL_FORMAT_YUV_SEMIPLANAR_420;
 	    stVpssChnMode.u32Width       = stSize.u32Width;
 	    stVpssChnMode.u32Height      = stSize.u32Height;
 	    stVpssChnMode.enCompressMode = COMPRESS_MODE_SEG;
@@ -308,10 +306,29 @@ void *SAMPLE_VENC_1080P_CLASSIC(HI_VOID *arg)
      step 5: start stream venc
     ******************************************/
     /*** HD1080P **/
-    enRcMode = SAMPLE_RC_CBR;
+    printf("\t c) cbr.\n");
+    printf("\t v) vbr.\n");
+    printf("\t f) fixQp\n");
+    printf("please input choose rc mode!\n");
+    c = (char)getchar();
+    switch(c)
+    {
+        case 'c':
+            enRcMode = SAMPLE_RC_CBR;
+            break;
+        case 'v':
+            enRcMode = SAMPLE_RC_VBR;
+            break;
+        case 'f':
+            enRcMode = SAMPLE_RC_FIXQP;
+            break;
+        default:
+            printf("rc mode! is invaild!\n");
+            goto END_VENC_1080P_CLASSIC_4;
+    }
 
 	/*** enSize[0] **/
-	if(s32ChnNum >= 99)
+	if(s32ChnNum >= 1)
 	{
 		VpssGrp = 0;
 	    VpssChn = 0;
@@ -333,11 +350,10 @@ void *SAMPLE_VENC_1080P_CLASSIC(HI_VOID *arg)
 	}
 
 	/*** enSize[1] **/
-	if(s32ChnNum >= 1)
+	if(s32ChnNum >= 2)
 	{
-	    VpssGrp = 0;
-	    VpssChn = 1;
-	    VencChn = 0;
+		VpssChn = 1;
+	    VencChn = 1;
 	    s32Ret = SAMPLE_COMM_VENC_Start(VencChn, enPayLoad[1], \
 	                                    gs_enNorm, enSize[1], enRcMode,u32Profile);
 	    if (HI_SUCCESS != s32Ret)
@@ -387,6 +403,7 @@ void *SAMPLE_VENC_1080P_CLASSIC(HI_VOID *arg)
     getchar();
     getchar();
     g_s32Quit=-1;
+
 
     /******************************************
      step 7: exit process
@@ -445,7 +462,7 @@ END_VENC_1080P_CLASSIC_1:	//vi stop
 END_VENC_1080P_CLASSIC_0:	//system exit
     SAMPLE_COMM_SYS_Exit();
     
-    return;
+    return; 
 }
 
 
@@ -492,7 +509,7 @@ HI_S32 SAMPLE_VENC_1080P_MJPEG_JPEG(HI_VOID)
                 enSize, SAMPLE_PIXEL_FORMAT, SAMPLE_SYS_ALIGN_WIDTH);
 
     stVbConf.astCommPool[0].u32BlkSize = u32BlkSize;
-    stVbConf.astCommPool[0].u32BlkCnt = g_u32RTMPBlkCnt;
+    stVbConf.astCommPool[0].u32BlkCnt = g_u32BlkCnt;
 
 
     /******************************************
@@ -1520,12 +1537,6 @@ END_VENC_1080P_CLASSIC_0:	//system exit
 	return s32Ret;	  
 }
 
-
-// Here must be some function called SAMPLE_VENC_H264_IntraRefresh.
-// And implementation of RTMP push mechanism has modifications in it.
-// Probably those modifications are intended to change frequency of I-frames.
-// But this SDK does not have it. This fact may cause some kind of problems.
-
 /******************************************************************************
 * function :  Thumbnail of 1*1080p jpeg
 ******************************************************************************/
@@ -1752,6 +1763,59 @@ END_VENC_MJPEG_JPEG_0:	//system exit
     SAMPLE_COMM_SYS_Exit();
     
     return s32Ret;
+}
+
+
+
+/******************************************************************************
+* function    : main()
+* Description : video venc sample
+******************************************************************************/
+int main(int argc, char *argv[])
+{
+    HI_S32 s32Ret;
+    if ( (argc < 2) || (1 != strlen(argv[1])))
+    {
+        SAMPLE_VENC_Usage(argv[0]);
+        return HI_FAILURE;
+    }
+
+    signal(SIGINT, SAMPLE_VENC_HandleSig);
+    signal(SIGTERM, SAMPLE_VENC_HandleSig);
+    
+    switch (*argv[1])
+    {
+        case '0':/* H.264@1080p@30fps+H.265@1080p@30fps+H.264@D1@30fps */
+            s32Ret = SAMPLE_VENC_1080P_CLASSIC();
+            break;
+        case '1':/* 1*1080p mjpeg encode + 1*1080p jpeg  */
+            s32Ret = SAMPLE_VENC_1080P_MJPEG_JPEG();
+            break;
+        case '2':/* low delay */
+            s32Ret = SAMPLE_VENC_LOW_DELAY();
+            break;
+        case '3':/* roibg framerate */
+            s32Ret = SAMPLE_VENC_ROIBG_CLASSIC();
+            break;
+        case '4':/* Thumbnail of 1*1080p jpeg  */
+            s32Ret = SAMPLE_VENC_1080P_JPEG_Thumb();
+            break;
+#ifndef hi3518ev201			
+		case '5':/* H.264 Svc-t */
+			s32Ret = SAMPLE_VENC_SVC_H264();
+			break;
+#endif
+        default:
+            printf("the index is invaild!\n");
+            SAMPLE_VENC_Usage(argv[0]);
+            return HI_FAILURE;
+    }
+    
+    if (HI_SUCCESS == s32Ret)
+        printf("program exit normally!\n");
+    else
+        printf("program exit abnormally!\n");
+    exit(s32Ret);
 }
 
 #ifdef __cplusplus
